8fbd7a9709ad3cee29fee4e64106278f1bf03d62
Leveditor <leveditorfc@gmail.com>
2021-02-03 11:34:09 -0300

    Enviando o primeiro commit do projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import cronometro from './assets/cronometro.png'
+import './estilo.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            numero: 0,
+            botaoText: 'VAI'
+        };
+        this.timer = null;
+    }
+
+    iniciar = () => {
+        let state = this.state;
+
+        if (this.timer !== null) {
+            // INICIAR O CRONOMETRO
+            clearInterval(this.timer);
+            this.timer = null;
+            state.botaoText = 'INICIAR';
+
+        } else {
+            // PAUSAR O CRONOMETRO
+            this.timer = setInterval(() => {
+                let state = this.state;
+                state.numero += 0.1;
+                this.setState(state);
+            }, 100);
+            state.botaoText = 'PAUSAR';
+        }
+
+        this.setState(state);
+    }
+
+    //LIMPAR O CRONOMETRO
+    zerar = () => {
+        if (this.timer !== null) {
+            clearInterval(this.timer);
+            this.timer = null;
+        }
+
+        let state = this.state;
+        state.numero = 0;
+        state.botaoText = 'INICIAR';
+        this.setState(state);
+    }
+
+    render() {
+
+        return (
+            <div className="container" >
+
+                {/* IMAGEM DO CRONOMETRO */}
+                <img src={cronometro} className="img" />
+
+                {/* MINUTOS DO CRONOMETRO */}
+                <a className="timer">{this.state.numero.toFixed(2)}</a>
+
+                {/* BOTÕES DE INICIAR E ZERAR CRONOMETRO */}
+                <div className="areaBtn">
+                    <a className="botao" onClick={this.iniciar}>{this.state.botaoText}</a>
+                    <a className="botao" onClick={this.zerar}>ZERAR</a>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
